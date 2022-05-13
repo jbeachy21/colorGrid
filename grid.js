d896767b73fd663fdf58b3b4776234e776e422e5
Jasper Beachy <jbeach17@gmail.com>
2022-05-13 15:31:42 -0500

    time constraint implemented

--- grid.js
+++ grid.js
@@ -1,29 +1,52 @@
 var recentlyChanged = [];
 
-function eligible() {
-    recentlyChanged.pop();
-    return recentlyChanged;
-}
+
 function changeColor() {
-    setTimeout(eligible, 2000);
-   
+    
     let ids = ["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16"];
-    let random = Math.floor(Math.random() * 17);
+    let random = Math.floor(Math.random() * 16);
     let colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF", "#C0C0C0", "#660099", "#ff9900"];
     let whichColor = Math.floor(Math.random() * 10);
     let id = ids[random];
     let color = colors[whichColor];
+    console.log(`Before everything : ${id} and recentlyChanged: ${recentlyChanged[recentlyChanged.length-1]}`);
+    if(id === recentlyChanged[recentlyChanged.length - 1]) {
+        console.log(`Inside if block id: ${id} and recentlyChanged: ${recentlyChanged[recentlyChanged.length-1]}`);
+        recentlyChanged.pop();
+        
+    }
+    else {
+        console.log(`Inside else blockid: ${id} and recentlyChanged: ${recentlyChanged[recentlyChanged.length-1]}`);
     
-    let box = document.getElementById(id)["style"];
-    console.log(box["backgroundColor"])
+        
     
-    box["backgroundColor"] = color
+        let box = document.getElementById(id);
+        // console.log(box["backgroundColor"])
+        
     
-    recentlyChanged.push(ids[random]); 
-    return recentlyChanged;
+        box["style"]["backgroundColor"] = color
+    
+        recentlyChanged.push(id); 
+        return recentlyChanged;
+    }
+    setTimeout(function() {
+        console.log(`Outside control statement id: ${id} and recentlyChanged: ${recentlyChanged[recentlyChanged.length-1]}`);
+    
+        
+        
+        let box = document.getElementById(id);
+        // console.log(box["backgroundColor"])
+        
+        
+        box["style"]["backgroundColor"] = color
+        
+        recentlyChanged.push(id); 
+        return recentlyChanged;
+        
+
+    },2000)
+    
+    
 }
 setInterval(changeColor, 250);
  
-
-
-
