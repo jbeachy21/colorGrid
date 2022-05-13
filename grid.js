4ed9bf1f093c44d9f9b1092fa837e0dd535491ba
Jasper Beachy <jbeach17@gmail.com>
2022-05-13 11:26:38 -0500

    color changed fixed

--- grid.js
+++ grid.js
@@ -8,9 +8,9 @@ function changeColor() {
     setTimeout(eligible, 2000);
    
     let ids = ["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","c11","c12","c13","c14","c15","c16"];
-    let random = Math.ceil((Math.random() * 16));
-    let colors = ["#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF", "#C0C0C0", "#FFFFFF"];
-    let whichColor = Math.ceil((Math.random() * 9));
+    let random = Math.floor(Math.random() * 17);
+    let colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#00FFFF", "#FF00FF", "#C0C0C0", "#660099", "#ff9900"];
+    let whichColor = Math.floor(Math.random() * 10);
     let id = ids[random];
     let color = colors[whichColor];
     
